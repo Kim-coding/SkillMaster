0355b94c68968a93172ea3a8683f681e9253d48c
김성진 <helloksj12@gmail.com>
2024-07-05 13:54:14 +0900

    Attack 기능 작업

---
+++ Assets/SJ/Attack.cs
@@ -0,0 +1,14 @@
+public class Attack
+{
+    public BigInteger Damage { get; set; }
+    public bool Critical { get; set; }
+
+    //public AttackType Type { get; set; }
+
+    public Attack(BigInteger damage, bool critical /* AttackType type*/ )
+    {
+        Damage = damage;
+        Critical = critical;
+        //Type = type;
+    }
+}
