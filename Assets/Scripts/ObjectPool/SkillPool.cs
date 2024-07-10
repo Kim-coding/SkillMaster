ab6b3ebb93ada2dfd550d75e87a35f11db143fe8
김보현 <qhgus5459@naver.com>
2024-07-10 18:24:29 +0900

    SkillPool 주석 추가

--- Assets/Scripts/ObjectPool/SkillPool.cs
+++ Assets/Scripts/ObjectPool/SkillPool.cs
@@ -18,4 +18,8 @@ public class SkillPool : ObjectPool<SkillBase>
     {
         base.OnReturn(obj);
     }
+    //SkillProjectile를 SkillBase를 상속 받게 하면 되지 않을까? 라는 생각 중
+    //SkillBase, Skill 스크립트 추가
+    //SkillBase는 추상 클래스로 만들었음.
+    //SkillBase를 상속받아 구현을 하면 PlayerSkills 에서 뻑날예정 수정이 필요 함.
 }
