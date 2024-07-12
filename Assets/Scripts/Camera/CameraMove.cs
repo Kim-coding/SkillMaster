54f225e5c5f2b0c981b20767facf9629b778db4c
김보현 <qhgus5459@naver.com>
2024-07-12 17:44:32 +0900

    카메라 확대 축소 수정

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -1,19 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CameraMove : MonoBehaviour
 {
     public GameObject player;
-    public float speed = 0.125f;
+    public float moveDuration = 0.5f; // 카메라 이동 시간
+    public float zoomDuration = 0.5f; // 카메라 줌 시간
     public float YPosition;
     public float[] distances;
     public Button CameraButton;
 
     private int currentDistanceIndex = 0;
     private Camera cam;
+    private float initialOrthographicSize;
+    private Vector3 initialPosition;
 
     private void Start()
     {
@@ -22,6 +23,8 @@ public class CameraMove : MonoBehaviour
         if (distances.Length > 0)
         {
             cam.orthographicSize = distances[currentDistanceIndex];
+            initialOrthographicSize = cam.orthographicSize;
+            initialPosition = transform.position;
         }
     }
 
@@ -29,20 +32,28 @@ public class CameraMove : MonoBehaviour
     {
         MoveCamera();
     }
+
     public void ChangeCameraDistance()
     {
         currentDistanceIndex = (currentDistanceIndex + 1) % distances.Length;
-        cam.orthographicSize = distances[currentDistanceIndex];
+        float targetOrthographicSize = distances[currentDistanceIndex];
+
+        cam.DOOrthoSize(targetOrthographicSize, zoomDuration);
         MoveCamera();
     }
 
     private void MoveCamera()
     {
         Vector3 dir = player.transform.position;
-        dir.y -= YPosition;
+        dir.y -= YPosition * (cam.orthographicSize / initialOrthographicSize);
         Vector3 desiredPosition = dir;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
-        smoothedPosition.z = transform.position.z;
-        transform.position = smoothedPosition;
+        desiredPosition.z = initialPosition.z;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, moveDuration);
+    }
+    private Vector3 GetTargetPosition(float targetOrthographicSize)
+    {
+        Vector3 dir = player.transform.position;
+        dir.y -= YPosition * (targetOrthographicSize / initialOrthographicSize);
+        return new Vector3(dir.x, dir.y, initialPosition.z);
     }
 }
