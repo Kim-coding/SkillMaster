206adc0214e48a58587881fcd46dced6a903cb06
김성진 <helloksj12@gmail.com>
2024-07-16 15:07:41 +0900

    몬스터 에셋 적용/ 카메라 클램프/카메라 모드 변경

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -1,10 +1,12 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
+using System.ComponentModel;
 
 public class CameraMove : MonoBehaviour
 {
-    public GameObject player;
+
     public float moveDuration = 0.5f; // 카메라 이동 시간
     public float zoomDuration = 0.5f; // 카메라 줌 시간
     public float YPosition;
@@ -12,71 +14,65 @@ public class CameraMove : MonoBehaviour
     public Button CameraButton;
 
     private int currentDistanceIndex = 0;
-    private Camera cam;
-    private float initialOrthographicSize;
-    private Vector3 initialPosition;
+    public Button toggleWindowButton;
+    [HideInInspector]
+    public bool isToggle = true;
 
-    public bool isToggle = false;
+    public CinemachineVirtualCamera centerCameraNear;
+    public CinemachineVirtualCamera centerCameraMiddle;
+    public CinemachineVirtualCamera centerCameraFar;
+    public CinemachineVirtualCamera topCameraNear;
+    public CinemachineVirtualCamera topCameraMiddle;
+    public CinemachineVirtualCamera topCameraFar;
+
 
     private void Start()
     {
-        cam = GetComponent<Camera>();
         CameraButton.onClick.AddListener(ChangeCameraDistance);
-        if (distances.Length > 0)
-        {
-            cam.orthographicSize = distances[currentDistanceIndex];
-            initialOrthographicSize = cam.orthographicSize;
-            initialPosition = transform.position;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if(!isToggle)
-        {
-            MoveCamera();
-        }
-        else
-        {
-            CloseMoveCamera();
-        }
+        toggleWindowButton.onClick.AddListener(() => { isToggle = !isToggle; ChangeCameraDistance(); });
+        ChangeCameraDistance();
     }
 
     public void ChangeCameraDistance()
     {
         currentDistanceIndex = (currentDistanceIndex + 1) % distances.Length;
-        float targetOrthographicSize = distances[currentDistanceIndex];
 
-        cam.DOOrthoSize(targetOrthographicSize, zoomDuration);
+        centerCameraNear.gameObject.SetActive(false);
+        centerCameraMiddle.gameObject.SetActive(false);
+        centerCameraFar.gameObject.SetActive(false);
+        topCameraNear.gameObject.SetActive(false);
+        topCameraMiddle.gameObject.SetActive(false);
+        topCameraFar.gameObject.SetActive(false);
+
         if (!isToggle)
         {
-            MoveCamera();
+            switch (currentDistanceIndex)
+            {
+                case 0:
+                    centerCameraNear.gameObject.SetActive(true);
+                    break;
+                case 1:
+                    centerCameraMiddle.gameObject.SetActive(true);
+                    break;
+                case 2:
+                    centerCameraFar.gameObject.SetActive(true);
+                    break;
+            }
         }
         else
         {
-            CloseMoveCamera();
+            switch (currentDistanceIndex)
+            {
+                case 0:
+                    topCameraNear.gameObject.SetActive(true);
+                    break;
+                case 1:
+                    topCameraMiddle.gameObject.SetActive(true);
+                    break;
+                case 2:
+                    topCameraFar.gameObject.SetActive(true);
+                    break;
+            }
         }
-    }
-
-    private void MoveCamera()
-    {
-        Vector3 dir = player.transform.position;
-        dir.y -= YPosition * (cam.orthographicSize / initialOrthographicSize);
-        Vector3 desiredPosition = dir;
-        desiredPosition.z = initialPosition.z;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, moveDuration);
-    }
-    private Vector3 GetTargetPosition(float targetOrthographicSize)
-    {
-        Vector3 dir = player.transform.position;
-        dir.y -= YPosition * (targetOrthographicSize / initialOrthographicSize);
-        return new Vector3(dir.x, dir.y, initialPosition.z);
-    }
-    private void CloseMoveCamera()
-    {
-        Vector3 dir = player.transform.position;
-        Vector3 desiredPosition = dir;
-        desiredPosition.z = initialPosition.z;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, moveDuration);
     }
 }
