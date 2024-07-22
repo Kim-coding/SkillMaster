6dfde1498ea7e50021e2b65d9c5c7c31d261673f
김보현 <qhgus5459@naver.com>
2024-07-22 12:09:14 +0900

    Missing오류 수정

--- Assets/Scripts/Scene/MainScene.cs
+++ Assets/Scripts/Scene/MainScene.cs
@@ -75,6 +75,10 @@ public class MainScene : MonoBehaviour
                     RemoveMonsters(monsterAI.gameObject);
                 }
             }
+            else
+            {
+                RemoveMonsters(monster.gameObject);
+            }
         }
     }
 
