2bcd8f10d0447ab13ec57fc706ccea295d4c0e58
김보현 <qhgus5459@naver.com>
2024-08-12 10:45:04 +0900

    SandBag Dungeon Test

--- Assets/Scripts/Scene/DungeonScene.cs
+++ Assets/Scripts/Scene/DungeonScene.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class DungeonScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject GoldDungeonMonster;
+    public GameObject[] DiaDungeonMonsters;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
