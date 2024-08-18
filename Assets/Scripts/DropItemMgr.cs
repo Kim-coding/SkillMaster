09c5466a4741b2b8c2acccc711627692d4c6e04b
김보현 <qhgus5459@naver.com>
2024-08-18 17:57:10 +0900

    goldPool

---
+++ Assets/Scripts/DropItemMgr.cs
@@ -0,0 +1,36 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemMgr : MonoBehaviour
+{
+    public RectTransform uiGoldTarget;
+    public RectTransform uiDiaTarget;
+    public GameObject goldPrefab;
+    public GameObject diaPrefab;
+    public Canvas uiCanvas;
+
+    public void CreateAndMoveGold(Vector3 startPosition, float duration, string value)
+    {
+        GameObject goldObject = Instantiate(goldPrefab, uiCanvas.transform);
+        RectTransform goldRectTransform = goldObject.GetComponent<RectTransform>();
+
+        goldRectTransform.anchoredPosition = startPosition;
+
+        DropItemMovement goldMovement = goldObject.GetComponent<DropItemMovement>();
+        goldMovement.Initialize(uiGoldTarget, duration, value, true);
+    }
+
+    public void CreateAndMoveDia(Vector3 startPosition, float duration, string value)
+    {
+        GameObject goldObject = Instantiate(diaPrefab, uiCanvas.transform);
+        RectTransform goldRectTransform = goldObject.GetComponent<RectTransform>();
+
+        goldRectTransform.anchoredPosition = startPosition;
+
+        DropItemMovement diaMovement = goldObject.GetComponent<DropItemMovement>();
+        Debug.Log(value);
+        diaMovement.Initialize(uiDiaTarget, duration, value, false);
+    }
+}
