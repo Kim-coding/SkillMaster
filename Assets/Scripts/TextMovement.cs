aca8aa1349eda85b01235dbbdf3750e460e1e2f8
홍지영 <wlduddl54321@gmail.com>
2024-08-28 18:16:21 +0900

    start Text 수정

--- Assets/Scripts/TextMovement.cs
+++ Assets/Scripts/TextMovement.cs
@@ -5,7 +5,7 @@ using TMPro;
 public class TextMovement : MonoBehaviour
 {
     public GameObject startPanel;
-    private float speed = 300f;
+    private float speed = 700f;
     private float duration = 0.25f;
 
     private RectTransform rectTransform;
@@ -25,7 +25,7 @@ public class TextMovement : MonoBehaviour
         //SetUpdate : DOTween을 TimeScale의 영향을 받지 않도록 설정
         sequence.Append(rectTransform.DOAnchorPosX(0, movespeed).SetEase(Ease.Linear)).SetUpdate(true); 
 
-        sequence.AppendInterval(duration);
+        //sequence.AppendInterval(duration);
 
         sequence.Append(rectTransform.DOAnchorPosX(screenWidth + rectTransform.rect.width, movespeed).SetEase(Ease.Linear).SetUpdate(true)).OnComplete(End);
     }
