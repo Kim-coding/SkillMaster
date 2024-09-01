b2c14fa156478ed67cea84b37e0ff80b8f8b8013
김보현 <qhgus5459@naver.com>
2024-09-02 00:28:02 +0900

    정보창 수정

--- Assets/Scripts/Help.cs
+++ Assets/Scripts/Help.cs
@@ -4,18 +4,32 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum InfoStringID
+{
+    info47 = 120547,
+    info48,
+    info49,
+    info50,
+    info51,
+    info52,
+    info53,
+    info54,
+    info55,
+}
+
 public class Help : MonoBehaviour
 {
     public GameObject infoPopupPanel;
     public TextMeshProUGUI infoPopupText;
+    public InfoStringID infoStringID;
     public bool imageInfo = false;
 
-    public void OpendPopup(int stringID)
+    public void OpendPopup()
     {
         infoPopupPanel.SetActive(true);
         if (!imageInfo)
         {
-            infoPopupText.text = DataTableMgr.Get<StringTable>(DataTableIds.String).GetID(stringID);
+            infoPopupText.text = DataTableMgr.Get<StringTable>(DataTableIds.String).GetID((int)infoStringID);
         }
     }
 
